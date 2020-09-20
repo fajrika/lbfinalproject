bc468636f6f42428ea5a03a910d61634b179104d
fajrika <42020056+fajrika@users.noreply.github.com>
2020-09-21 00:18:41 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::namespace('App\Http\Controllers')->group(function(){
     Route::get('userManagement','api\UserManagementAPI@__invoke');
-    Route::get('item','api\ItemAPI');
-    Route::get('customer','api\CustomerAPI');
-    Route::get('supplier','api\SupplierAPI');
-    Route::get('category','api\CategoryAPI');
-    Route::get('incomingItem','api\IncomingItemAPI');
-    Route::get('outcomingItem','api\OutcomingItemAPI');
+    Route::get('item','api\ItemAPI@__invoke');
+    Route::get('customer','api\CustomerAPI@__invoke');
+    Route::get('supplier','api\SupplierAPI@__invoke');
+    Route::get('category','api\CategoryAPI@__invoke');
+    Route::get('incomingItem','api\IncomingItemAPI@__invoke');
+    Route::get('outcomingItem','api\OutcomingItemAPI@__invoke');
     // Route::get('itemFlow','api\ItemFlowAPI');
 });
 // Route::middleware('auth:api')->get('/user', function (Request $request) {
